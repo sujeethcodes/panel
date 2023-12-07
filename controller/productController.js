210aa24d88d3e2e1cfdf4bc3f132b18476a87567
sujeeth.s <sujeeth.s@synamen.com>
2023-12-07 14:55:31 +0530

    checkProduct

--- controller/productController.js
+++ controller/productController.js
@@ -4,6 +4,10 @@ const controller = {}
 
 controller.createProducts = async(req, res)=>{
 
+    if(!req.body.product) throw "PRODUCT_REQUIRED"
+    if(!req.body.quantity) throw "QUANTITY_REQUIRED"
+    if(!req.body.price) throw "PRICE_REQUIRED"
+
    const createProducts = await Products.create({
     productId: utils.getRandomNumber(100),
     productName:req?.body?.product,
@@ -14,6 +18,8 @@ controller.createProducts = async(req, res)=>{
 
    if(createProducts){
     res.json("productCreate")
+}else{
+    res?.json("SOMETHING_WENT_WRONG")
 }
 
 }
